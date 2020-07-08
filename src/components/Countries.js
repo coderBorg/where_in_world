8bd5b36e5a0c5db7d0704fd25c4aa50b8f1c1825
Jeff Borg <jeff.borg8@gmail.com>
2020-07-08 11:41:11 -0700

    Add Countries and CountryItem components

---
+++ src/components/Countries.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CountryItem from './CountryItem'
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Countries 2</h1>
+            <p>Countries here</p>
+            <CountryItem name='Norway'/>
+        </div>
+    );
+}
+
+export default Countries;
---
+++ src/components/CountryItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const CountryItem = ({name}) => {
+    return (
+        <div>
+            <p>Country item</p>
+            <p>Name: {name}</p>
+        </div>
+    );
+}
+
+export default CountryItem;
