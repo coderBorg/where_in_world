c645dfc119a5eec41326e2569c10b2e6d1a365d1
Jeff Borg <jeff.borg8@gmail.com>
2020-07-08 12:19:57 -0700

    Add temp countries data, add to country item

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,11 +2,17 @@ import React from 'react'
 import CountryItem from './CountryItem'
 
 const Countries = () => {
+
+    // Temporary countries data
+    let data = [{name: 'Norway', population: 1000, capital: 'Oslo'},
+            {name: 'Canada', population: 2000, capital: 'Ottawa'}]
+
     return (
         <div>
             <h1>Countries 2</h1>
             <p>Countries here</p>
-            <CountryItem name='Norway'/>
+            {data.map(country => (<CountryItem 
+                                    country={country} />))}
         </div>
     );
 }
--- src/components/CountryItem.js
+++ src/components/CountryItem.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export const CountryItem = ({name}) => {
+export const CountryItem = ({country: {name, capital, population}}) => {
     return (
         <div>
             <p>Country item</p>
             <p>Name: {name}</p>
+            <p>Capital: {capital}</p>
+            <p>Pop: {population}</p>
         </div>
     );
 }
