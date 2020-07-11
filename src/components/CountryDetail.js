a212467e89f0353b43d04c1eb73a73238fa073b4
Jeff Borg <jeff.borg8@gmail.com>
2020-07-10 22:00:25 -0500

    Make country name work in CountryDetail

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export const CountryDetail = () => {
+export const CountryDetail = ({match}) => {
   return (
     <div>
       <p>Country detail here</p>
+      <p>{match.params.name}</p>
     </div>
   );
 };
