e4e7e80d67d2e771a7cff9e65e07dc82f1b58805
Jeff Borg <jeff.borg8@gmail.com>
2020-07-21 16:21:08 -0700

    This way doesn't work either

--- src/context/countries/countriesReducer.js
+++ src/context/countries/countriesReducer.js
@@ -12,7 +12,17 @@ export default (state, action) => {
       tempState.countryDetail = action.payload;
       // tempState.countryDetail.borders = action.payload.borders;
       // tempState.countryDetail.borders = action.payload.borders.map((b) => {return b;});
-      tempState.countryDetail.borders = action.payload.borders.slice();
+      // tempState.countryDetail.borders = action.payload.borders.slice();
+      let tempBorders = [];
+      action.payload.borders.forEach(el => tempBorders.push(el));
+      
+      // debug
+      console.log('in reducer');
+      console.log("in reducer, borders[0]: ");
+      console.log(tempState.countryDetail.borders[0]);
+      tempState.countryDetail.borders.forEach(el => console.log(el));
+      
+      tempState.countryDetail.borders = tempBorders;
       return tempState;
   }
 };
