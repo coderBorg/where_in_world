e1bc30bfa175daec617d2e4e42f710310287f701
Jeff Borg <jeff.borg8@gmail.com>
2020-08-11 10:45:08 -0700

    Format Countries.js

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,12 +7,20 @@ import CountriesContext from '../context/countries/countriesContext';
 const Countries = () => {
   const countriesContext = useContext(CountriesContext);
 
-  const { searchText, countries, filterValue, filteredCountries, getAllCountries, updateFilteredCountries, searchAndFilter } = countriesContext;
+  const {
+    searchText,
+    countries,
+    filterValue,
+    filteredCountries,
+    getAllCountries,
+    updateFilteredCountries,
+    searchAndFilter,
+  } = countriesContext;
 
   useEffect(() => {
     // getAllCountries();
     // updateFilteredCountries(countries, filterValue);
-    searchAndFilter(searchText, filterValue)
+    searchAndFilter(searchText, filterValue);
     // eslint-disable-next-line
   }, []);
 
@@ -20,7 +28,7 @@ const Countries = () => {
     <div>
       <Search />
       <Filter />
-      <div style={{ display: 'flex', flexWrap: 'wrap'}}>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {filteredCountries.map((country) => (
           <CountryItem country={country} />
         ))}
