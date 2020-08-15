424acb5cf28a295058615be5cd33186c5e1e6141
Jeff Borg <jeff.borg8@gmail.com>
2020-08-14 18:03:44 -0700

    Revise CountryDetail layout and styling

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -41,33 +41,105 @@ export const CountryDetail = ({ match }) => {
           Back
         </Link>
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap'}}>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         <div>
-          <img src={countryDetail.flag} alt="" style={{ width: '400px'}} />
+          <img src={countryDetail.flag} alt="" style={{ width: '400px' }} />
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', textAlign: 'justify', margin: '20px 20px'}}>
-          <p style={{fontWeight: 'bold'}}>{name}</p>
-          <p>Native Name: {nativeName}</p>
-          <p>Population: {population}</p>
-          <p>Region: {region}</p>
-          <p>Sub Region: {subregion}</p>
-          <p>Capital: {capital}</p>
-          <p>Top Level Domain: {topLevelDomain}</p>
-          <p>Currencies:</p>
-          {countryDetailCurrencies.map((cur) => (
-            <p>{cur}</p>
-          ))}
-          <p>Languages</p>
-          {countryDetailLanguages.map((cur) => (
-            <p>{cur}</p>
-          ))}
-          <p>Border Countries:</p>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            textAlign: 'justify',
+            margin: '20px 20px',
+          }}
+        >
           <div
-            style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row' }}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              textAlign: 'justify',
+              margin: '20px 20px',
+            }}
           >
-            {countryDetailBorders.map((name) => (
-              <BorderCountryItem name={name} />
-            ))}
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                textAlign: 'justify',
+                margin: '20px 20px',
+              }}
+            >
+              <p style={{ fontWeight: 'bold' }}>{name}</p>
+              <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                textAlign: 'justify',
+                margin: '20px 20px',
+              }}>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-start',
+                    textAlign: 'justify',
+                    margin: '20px 20px',
+                  }}
+                >
+                  <p>Native Name: {nativeName}</p>
+                  <p>Population: {population}</p>
+                  <p>Region: {region}</p>
+                  <p>Sub Region: {subregion}</p>
+                  <p>Capital: {capital}</p>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-start',
+                    textAlign: 'justify',
+                    margin: '20px 20px',
+                  }}
+                >
+                  <p>Top Level Domain: {topLevelDomain}</p>
+                  <p>Currencies:</p>
+                  {countryDetailCurrencies.map((cur) => (
+                    <p>{cur}</p>
+                  ))}
+                  <p>Languages:</p>
+                  {countryDetailLanguages.map((cur) => (
+                    <p>{cur}</p>
+                  ))}
+                </div>
+              </div>
+              <div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  flexDirection: 'row',
+                }}
+              >
+                <p>Border Countries:</p>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    flexDirection: 'row',
+                  }}
+                >
+                  {countryDetailBorders.map((name) => (
+                    <BorderCountryItem name={name} />
+                  ))}
+                </div>
+              </div>
+            </div>
+            </div>
+
+            
           </div>
         </div>
       </div>
