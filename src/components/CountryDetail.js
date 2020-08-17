c07a57f31330c61930f91890753fec642455c2b4
Jeff Borg <jeff.borg8@gmail.com>
2020-08-17 14:28:26 -0700

    Get rid of color for visited links, center coutry detail, etc

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -41,23 +41,15 @@ export const CountryDetail = ({ match }) => {
           <p>Back</p>
         </div>
       </Link>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        <div>
-          <img src={countryDetail.flag} alt="" style={{ height: '300px' }} />
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-start',
-            textAlign: 'justify',
-            margin: '0px 20px',
-          }}
-        >
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap'}}>
+          <div>
+            <img src={countryDetail.flag} alt="" style={{ height: '300px' }} />
+          </div>
           <div
             style={{
               display: 'flex',
-              flexDirection: 'row',
+              flexDirection: 'column',
               justifyContent: 'flex-start',
               textAlign: 'justify',
               margin: '0px 20px',
@@ -66,87 +58,88 @@ export const CountryDetail = ({ match }) => {
             <div
               style={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-around',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
                 textAlign: 'justify',
                 margin: '0px 20px',
-                height: '300px',
               }}
             >
-              <p style={{ fontWeight: 'bold' }}>{name}</p>
               <div
                 style={{
                   display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'flex-start',
+                  flexDirection: 'column',
+                  justifyContent: 'space-around',
                   textAlign: 'justify',
-                  margin: '0px 0px',
+                  margin: '0px 20px',
+                  height: '300px',
                 }}
               >
+                <p style={{ fontWeight: 'bold' }}>{name}</p>
                 <div
                   style={{
                     display: 'flex',
-                    flexDirection: 'column',
+                    flexDirection: 'row',
                     justifyContent: 'flex-start',
                     textAlign: 'justify',
                     margin: '0px 0px',
                   }}
                 >
-                  <p>
-                    <span className="ctry-detail-fld-name">Native Name:</span>{' '}
-                    {nativeName}
-                  </p>
-                  <p>
-                    <span className="ctry-detail-fld-name">Population:</span>{' '}
-                    {population}
-                  </p>
-                  <p>
-                    <span className="ctry-detail-fld-name">Region:</span>{' '}
-                    {region}
-                  </p>
-                  <p>
-                    <span className="ctry-detail-fld-name">Sub Region:</span>{' '}
-                    {subregion}
-                  </p>
-                  <p>
-                    <span className="ctry-detail-fld-name">Capital:</span>{' '}
-                    {capital}
-                  </p>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'flex-start',
+                      textAlign: 'justify',
+                      margin: '0px 0px',
+                    }}
+                  >
+                    <p>
+                      <span className="ctry-detail-fld-name">Native Name:</span>{' '}
+                      {nativeName}
+                    </p>
+                    <p>
+                      <span className="ctry-detail-fld-name">Population:</span>{' '}
+                      {population}
+                    </p>
+                    <p>
+                      <span className="ctry-detail-fld-name">Region:</span>{' '}
+                      {region}
+                    </p>
+                    <p>
+                      <span className="ctry-detail-fld-name">Sub Region:</span>{' '}
+                      {subregion}
+                    </p>
+                    <p>
+                      <span className="ctry-detail-fld-name">Capital:</span>{' '}
+                      {capital}
+                    </p>
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'flex-start',
+                      textAlign: 'justify',
+                      margin: '0px 40px',
+                    }}
+                  >
+                    <p>
+                      <span className="ctry-detail-fld-name">
+                        Top Level Domain:
+                      </span>{' '}
+                      {topLevelDomain}
+                    </p>
+                    <p>
+                      <span className="ctry-detail-fld-name">Currencies:</span>{' '}
+                      {countryDetailCurrencies.join(', ')}
+                    </p>
+                    <p>
+                      <span className="ctry-detail-fld-name">Languages:</span>{' '}
+                      {countryDetailLanguages.join(', ')}
+                    </p>
+                  </div>
                 </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'flex-start',
-                    textAlign: 'justify',
-                    margin: '0px 40px',
-                  }}
-                >
-                  <p>
-                    <span className="ctry-detail-fld-name">
-                      Top Level Domain:
-                    </span>{' '}
-                    {topLevelDomain}
-                  </p>
-                  <p>
-                    <span className="ctry-detail-fld-name">Currencies:</span>{' '}
-                    {countryDetailCurrencies.join(', ')}
-                  </p>
-                  <p>
-                    <span className="ctry-detail-fld-name">Languages:</span>{' '}
-                    {countryDetailLanguages.join(', ')}
-                  </p>
-                </div>
-              </div>
-              <div>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    flexDirection: 'row',
-                  }}
-                >
-                  <p>Border Countries:</p>
+                <div>
                   <div
                     style={{
                       display: 'flex',
@@ -154,9 +147,18 @@ export const CountryDetail = ({ match }) => {
                       flexDirection: 'row',
                     }}
                   >
-                    {countryDetailBorders.map((name) => (
-                      <BorderCountryItem name={name} />
-                    ))}
+                    <p className="ctry-detail-fld-name">Border Countries:</p>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        flexDirection: 'row',
+                      }}
+                    >
+                      {countryDetailBorders.map((name) => (
+                        <BorderCountryItem name={name} />
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
