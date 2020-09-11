62654211ed3fd8544bdf8c0fb5003f05c77a15ce
Jeff Borg <jeff.borg8@gmail.com>
2020-09-10 17:20:27 -0700

    Create CountryDetail.css and move some styling into it
    This makes CountryDetail.js more readable

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -7,23 +7,19 @@ import CountryDetailFields1 from './CountryDetailFields1';
 import CountryDetailFields2 from './CountryDetailFields2';
 import CountryDetailBorders from './CountryDetailBorders';
 
+import './CountryDetail.css';
+
 export const CountryDetail = ({ match }) => {
   const countriesContext = useContext(CountriesContext);
 
-  const {
-    darkMode,
-    countryDetail,
-    getCountryDetail,
-  } = countriesContext;
+  const { darkMode, countryDetail, getCountryDetail } = countriesContext;
 
   useEffect(() => {
     getCountryDetail(match.params.name);
     // eslint-disable-next-line
   }, [countryDetail]);
 
-  const {
-    name: countryName,
-  } = countryDetail;
+  const { name: countryName } = countryDetail;
 
   let cssBtnClass = darkMode ? 'dark btn-dark' : 'light btn-light';
   let cssDarkModeClass = darkMode ? 'dark' : 'light';
@@ -35,38 +31,14 @@ export const CountryDetail = ({ match }) => {
           <p>Back</p>
         </div>
       </Link>
-      <div
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'space-around',
-        }}
-      >
+      <div className="flex_row_overall">
         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
           <div>
             <img src={countryDetail.flag} alt="" style={{ height: '300px' }} />
           </div>
-
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-around',
-              textAlign: 'justify',
-              margin: '0px 60px',
-              height: '300px',
-            }}
-          >
+          <div className="flex_col_dtl_info">
             <p style={{ fontWeight: 'bold' }}>{countryName}</p>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'flex-start',
-                textAlign: 'justify',
-                margin: '0px 0px',
-              }}
-            >
+            <div className="flex_row_dtl_fields">
               <CountryDetailFields1 />
               <CountryDetailFields2 />
             </div>
