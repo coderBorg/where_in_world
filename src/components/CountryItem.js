902f523731d84deb95be9b3f6e971c54608b5de8
Jeff Borg <jeff.borg8@gmail.com>
2020-09-11 13:50:36 -0700

    Add div to help with styling

--- src/components/CountryItem.js
+++ src/components/CountryItem.js
@@ -16,30 +16,44 @@ export const CountryItem = ({
     margin: '0px 0px',
     padding: '0px 10px',
     width: '267px',
-    height: '176px'
+    height: '176px',
   };
 
   return (
-    <Link to={`/CountryDetail/${countryName}`} style={{ textDecoration: 'none' }}>
-      <div style={{ width: '267px', height: '336px', textAlign: 'justify', margin: '20px 0px' }}>
+    <Link
+      to={`/CountryDetail/${countryName}`}
+      style={{ textDecoration: 'none' }}
+    >
+      <div
+        style={{
+          width: '267px',
+          height: '336px',
+          textAlign: 'justify',
+          margin: '20px 0px',
+        }}
+      >
         <img
           src={flag}
           alt=""
-          style={{  width: '267px', height: '160px' }}
+          style={{ width: '267px', height: '160px' }}
           className={cssClass}
         />
         <div className={cssClass} style={flexColFields}>
-          <p style={{ fontWeight: 'bold', margin: '20px 0px' }}>{countryName}</p>
-          <p>
-            <span className="ctry-detail-fld-name">Population:</span>{' '}
-            {population}
-          </p>
-          <p>
-            <span className="ctry-detail-fld-name">Region:</span> {region}
-          </p>
-          <p>
-            <span className="ctry-detail-fld-name">Capital:</span> {capital}
-          </p>
+          <div style={{margin: '0px 20px'}}>
+            <p style={{ fontWeight: 'bold', margin: '20px 0px' }}>
+              {countryName}
+            </p>
+            <p>
+              <span className="ctry-detail-fld-name">Population:</span>{' '}
+              {population}
+            </p>
+            <p>
+              <span className="ctry-detail-fld-name">Region:</span> {region}
+            </p>
+            <p>
+              <span className="ctry-detail-fld-name">Capital:</span> {capital}
+            </p>
+          </div>
         </div>
       </div>
     </Link>
